dec87d2184718f81b9fc461c472c009b8ba39851
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-11-11 21:27:06 +0300

    Initial commit

---
+++ src/main/java/ArrayList/CarArrayList.java
@@ -0,0 +1,36 @@
+package ArrayList;
+
+public class CarArrayList implements CarList {
+
+    private Car[] array = new Car[10];
+    private int size = 0;
+    @Override
+    public Car get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(Car car) {
+
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAt(int index) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}

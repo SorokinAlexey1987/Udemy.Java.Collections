901a34607884ebf0eca77bd964963ff887dc0385
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-11-11 21:27:06 +0300

    Initial commit

---
+++ src/test/java/ArrayList/CarListTest.java
@@ -0,0 +1,12 @@
+package ArrayList;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CarListTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}

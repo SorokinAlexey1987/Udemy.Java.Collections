fe0eca35ef44e247bf0939cb41c7a084f21ac0bf
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-11-12 21:10:50 +0300

    Initial commit

--- src/main/java/ArrayList/CarArrayList.java
+++ src/main/java/ArrayList/CarArrayList.java
@@ -48,9 +48,12 @@ public class CarArrayList implements CarList {
     @Override
     public boolean removeAt(int index) {
         checkIndex(index);
+        /*
         for(int i = index; i < size - 1; i++) {
             array[i] = array[i + 1];
         }
+        */
+        System.arraycopy(array, index + 1, array, index, size - 1- index);
         size--;
         return true;
     }

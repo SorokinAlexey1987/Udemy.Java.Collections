a745501dec800ac2c21c9b2406026829f78636a0
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-11-13 21:38:28 +0300

    Initial commit

--- src/main/java/LinkedList/CarLinkedList.java
+++ src/main/java/LinkedList/CarLinkedList.java
@@ -11,7 +11,7 @@ public class CarLinkedList implements CarList {
 
     @Override
     public Car get(int index) {
-        return null;
+        return getNode(index).value;
     }
 
     @Override
@@ -29,7 +29,7 @@ public class CarLinkedList implements CarList {
 
     @Override
     public void add(Car car, int index) {
-        if (index <0 || index > size) {
+        if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException();
         }
         if (index == size) {
@@ -40,17 +40,44 @@ public class CarLinkedList implements CarList {
         Node nodePrevious = nodeNext.previous;
         Node newNode = new Node(nodePrevious, car, nodeNext);
         nodeNext.previous = newNode;
-        nodePrevious.next = newNode;
+        if (nodePrevious != null) {
+            nodePrevious.next = newNode;
+        } else {
+            first = newNode;
+        }
+        size++;
     }
 
     @Override
-    public boolean remove(Car car) {
+    public boolean remove(Car car) {;
+        Node node = first;
+        for (int i = 0; i < size; i++) {
+            if (node.value.equals(car)) {
+                return removeAt(i);
+            }
+            node = node.next;
+        }
         return false;
     }
 
     @Override
     public boolean removeAt(int index) {
-        return false;
+        Node node = getNode(index);
+        Node nodeNext = node.next;
+        Node nodePrevious = node.previous;
+        if (nodeNext != null) {
+            nodeNext.previous = nodePrevious;
+        } else {
+            last = nodePrevious;
+        }
+        nodeNext.previous = nodePrevious;
+        if (nodePrevious != null) {
+            nodePrevious.next = nodeNext;
+        } else {
+            first = nodeNext;
+        }
+        size--;
+        return true;
     }
 
     @Override
@@ -60,7 +87,9 @@ public class CarLinkedList implements CarList {
 
     @Override
     public void clear() {
-
+        first = null;
+        last = null;
+        size = 0;
     }
 
     private static class Node {
@@ -76,6 +105,9 @@ public class CarLinkedList implements CarList {
     }
 
     private Node getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
         Node node = first;
         for (int i = 0; i < index; i++) {
             node = node.next;

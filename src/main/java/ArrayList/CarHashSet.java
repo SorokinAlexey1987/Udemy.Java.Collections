4bb635810b33db0c6484a43552477da486dedd2d
Sorokin Aleksei <alexey-sor@yandex.ru>
2022-11-20 02:02:10 +0300

    Initial commit

--- src/main/java/ArrayList/CarHashSet.java
+++ src/main/java/ArrayList/CarHashSet.java
@@ -11,12 +11,51 @@ public class CarHashSet implements CarSet {
     @Override
     public boolean add(Car car) {
         int position = getElementPosition(car, array.length);
-
+        if(array[position] == null) {
+            Entry entry = new Entry(car, null);
+            array[position] = entry;
+            size++;
+            return true;
+        } else {
+            Entry existedElement = array[position];
+            while (true) {
+                if (existedElement.value.equals(car)) {
+                    return false;
+                } else if (existedElement.next == null) {
+                    existedElement.next = new Entry(car, null);
+                    size++;
+                    return true;
+                } else {
+                    existedElement = existedElement.next;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public boolean remove(Car car) {
+        int position = getElementPosition(car, array.length);
+        if(array[position] == null) {
+            return false;
+        }
+        Entry secondLast = array[position];
+        Entry last = secondLast.next;
+        if (secondLast.value.equals(car)) {
+            array[position] = last;
+            size--;
+            return true;
+        }
+        while (last != null) {
+            if (last.value.equals(car)) {
+                secondLast.next = last.next;
+                size--;
+                return true;
+            } else {
+                secondLast = last;
+                last = last.next;
+            }
+        }
         return false;
     }
 
@@ -27,7 +66,8 @@ public class CarHashSet implements CarSet {
 
     @Override
     public void clear() {
-
+        array = new Entry[INITIAL_CAPACITY];
+        size = 0;
     }
 
     private int getElementPosition(Car car, int arrayLength) {

914fe06f3f9c7107ee481d268e346acea07f691d
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-01-04 17:08:17 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,17 +22,17 @@ public class Main {
         }
          */
 
-        Set<Car> cars = new TreeSet<>(new Comparator<Car>() {
+        Set<Integer> numbers = new TreeSet<>(new Comparator<Integer>() {
             @Override
-            public int compare(Car o1, Car o2) {
-                return o1.getBrand().compareTo(o2.getBrand());
+            public int compare(Integer o1, Integer o2) {
+                return -o1.compareTo(o2);
             }
         });
         for (int i = 0; i < 100; i++) {
-            cars.add(new Car("Brand " + i, i));
+            numbers.add((int)(Math.random() * 10));
         }
-        for (Car car : cars) {
-            System.out.println(car);
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
